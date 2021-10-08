c371a3f85b4f3808169f9f05d418e081b87af00f
musasazib <musasazib@gmail.com>
2021-10-09 03:32:05 +0600

    Private Route, Implement Auth Redirect

---
+++ src/components/Balance/Balance.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Balance = () => {
+    return (
+        <div>
+            <h2>Your Balance is Faka</h2>
+            <h1>$00.00</h1>
+            <h4>Taka poisha nai</h4>
+        </div>
+    );
+};
+
+export default Balance;
